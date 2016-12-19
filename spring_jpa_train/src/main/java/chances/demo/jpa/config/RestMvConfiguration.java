e6bea798210d5c8f13223448165e53805021d83b
Coffee <Coffee@Coffee-X260>
2016-12-19 16:21:09 +0800

    init

--- spring_jpa_train/src/main/java/chances/demo/jpa/config/RestMvConfiguration.java
+++ spring_jpa_train/src/main/java/chances/demo/jpa/config/RestMvConfiguration.java
@@ -1,11 +1,35 @@
 package chances.demo.jpa.config;
 
+import java.text.SimpleDateFormat;
+
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+
+import chances.demo.jpa.entity.Role;
+import chances.demo.jpa.entity.User;
 
 @EnableJpaRepositories
 @Import(RepositoryRestMvcConfiguration.class)
-public class RestMvConfiguration {
+public class RestMvConfiguration  extends RepositoryRestConfigurerAdapter{
+	private  final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+	@Override
+	public void configureJacksonObjectMapper(ObjectMapper objectMapper) {
+		SerializationConfig serializationConfig = objectMapper.getSerializationConfig().with(df);
+		
+		objectMapper.setConfig(serializationConfig);
+	}
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		super.configureRepositoryRestConfiguration(config);
+		config.exposeIdsFor(User.class,Role.class);
+	}
 
 }
